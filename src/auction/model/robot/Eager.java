0b43e24d0bbe67a77a3591d3409186070773c19b
Your Name <you@example.com>
2020-04-12 13:00:55 +0200

    add Robot

--- src/auction/model/robot/Eager.java
+++ src/auction/model/robot/Eager.java
@@ -7,5 +7,6 @@ public class Eager extends Strategy {
 	@Override
 	Bid createBid(User user, double amount) {
 		return new Bid(amount + 1, user);
+		
 	}
 }
--- src/auction/model/robot/Strategy.java
+++ src/auction/model/robot/Strategy.java
@@ -4,6 +4,5 @@ import auction.model.Bid;
 import auction.model.User;
 
 public abstract class Strategy {
-
 	abstract Bid createBid(User user, double amount);
 }
